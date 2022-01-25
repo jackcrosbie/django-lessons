60c82aa57ffdc4f63e5cbaa10aa0d33ed6c02a2f
jackcrosbie <jack.crosbie@mycit.ie>
2022-01-25 20:40:31 +0000

    learning Django

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
+"""
+list of imports
+"""
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
+"""
+imports
+"""
 from django.db import models
 
+
 # Create your models here.
+
+class Item(models.Model):
+    """
+    created item class
+    """
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
